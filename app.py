24d890d0805a52e6e021bc1dccbcdb07bdc9cf96
suno <0910.shj@gmail.com>
2022-11-02 14:26:11 +0900

    tmi page

--- app.py
+++ app.py
@@ -8,14 +8,30 @@ from pymongo import MongoClient
 client = MongoClient("mongodb+srv://test:sparta@cluster0.kfz2usy.mongodb.net/?retryWrites=true&w=majority",tlsCAFile=certifi.where())
 db = client.sparta
 
-
 @app.route('/')
 def home():
+  return render_template('index.html')
+
+@app.route('/tmi')
+def tmi():
+  return render_template('tmi.html')
+@app.route('/tmi/sung')
+def sung():
+  return render_template('sung.html')
+@app.route('/tmi/bae')
+def bae():
+  return render_template('bae.html')
+
+@app.route('/goal')
+def goal():
+  return render_template('goal.html')
+
+@app.route('/guestbook')
+def guestbook():
   return render_template('guestbookIndex.html')
 
-
-@app.route("/homework", methods=["POST"])
-def homework_post():
+@app.route("/guestbook/comments", methods=["POST"])
+def guestbook_post():
   name_receive = request.form["name_give"]
   comment_receive = request.form["comment_give"]
 
@@ -28,8 +44,8 @@ def homework_post():
   return jsonify({'msg': '감사합니다!'})
 
 
-@app.route("/homework", methods=["GET"])
-def homework_get():
+@app.route("/guestbook/comments", methods=["GET"])
+def guestbook_get():
   comment_list = list(db.homework.find({}, {'_id': False}))
   return jsonify({'comments': comment_list})
 
