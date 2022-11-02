256cd57d9d0368a42204e0d07e2ec9ba28b0c2c9
suno <0910.shj@gmail.com>
2022-11-02 18:36:13 +0900

    Add toggle button

--- js/index.js
+++ js/index.js
@@ -29,3 +29,19 @@ function dynamic(randomArr) {
 }
 
 dynamic(randomString());
+
+const nav = document.querySelector('.navbar__menu');
+const btns = document.querySelector('#btn');
+btns.addEventListener('click', toggle1);
+function toggle1() {
+  if (nav.style.display !== 'none') {
+    nav.style.display = 'none';
+  } else {
+    nav.style.display = 'flex';
+  }
+}
+// 변경
+window.onresize = function (event) {
+  var innerWidth = window.innerWidth;
+  innerWidth > '769' ? (nav.style.display = 'flex') : (nav.style.display = 'none');
+};
